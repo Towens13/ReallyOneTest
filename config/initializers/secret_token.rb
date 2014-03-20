1c19e9757062997266ad718e706cf877d2254abd
Tim Owens <timowens@me.com>
2014-03-19 19:44:40 -0700

    creating a new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ReallyOneTest::Application.config.secret_key_base = 'd234c28705a3cc15f4a01f33036190acb9386525fd0276a4596ac9fc5eab14a3392d499bfe37d01cde577ba78eb75a22863ac7f56da6c68aac5bc0f370100888'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ReallyOneTest::Application.config.session_store :cookie_store, key: '_ReallyOneTest_session'

432046de3847613c35dc0868b2c50fa511005421
Tim Owens <timowens@me.com>
2014-07-19 17:27:07 -0700

    fixing routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ ReallyOneTest::Application.routes.draw do
 
   get 'home' , to: 'application#home'
 
-    devise_for :users :controllers => { omniauth_callbacks: 'omniauth_callbacks'}
+    #devise_for :users :controllers => { omniauth_callbacks: 'omniauth_callbacks'}
 
   get '/users/:id/add_email' => 'users#add_email', via: [:get, :patch, :post], 
   :as => :add_user_email
